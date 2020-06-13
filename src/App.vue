1b16d66221e7a9662bb29c20cff7287259042f83
alssdev <alssdev3@gmail.com>
2020-06-13 12:35:44 -0600

    Integración de navBar y Barra de búsqueda

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,40 @@
 <template>
   <div id="app">
+    <header>
+      <nav class="navbar">
+        <span class="navbar-brand mb-0 h1">MARVEL APP</span>
+      </nav>
+      <div id="searchBar">
+        <div class="container ">
+          <div class="input-group">
+            <input
+              type="text"
+              class="form-control"
+              aria-label="Text input with dropdown button"
+            />
+            <div class="input-group-append">
+              <button
+                class="btn btn-outline-secondary dropdown-toggle"
+                type="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Dropdown
+              </button>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+                <div role="separator" class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Separated link</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
     <div class="container">
       <div class="row">
         <div
@@ -82,5 +117,9 @@ header {
 .navbar span {
   font-size: 30px;
   padding: 0px;
+}
+
+#searchBar {
+  margin-top: 30px;
 }
 </style>
