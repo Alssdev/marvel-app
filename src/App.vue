b5f2de8180b4db248e6e2679c8b8ea75ebb8506e
alssdev <alssdev3@gmail.com>
2020-06-13 09:26:05 -0600

    new config

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Marvel Gallery</h1>
+    <button @click="fetch">Consultar</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
+import env from './env';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  methods: {
+    fetch: function() {},
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/env.js
@@ -0,0 +1,4 @@
+export default {
+  publicKey: '797eba7a566f6af2d16986452b4d0d87',
+  privateKey: '01ebd73c0bcdd629ea29e801c297c2ac8afb373e',
+};
